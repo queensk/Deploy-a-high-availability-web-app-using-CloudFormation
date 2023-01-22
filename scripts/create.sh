ba284bb08d6452c0c3e8444ecfbb4b37388c8037
queensk <queenskisivuli@gmail.com>
2023-01-22 08:52:28 +0300

    fixed usage fille parametors

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name $1 --template-body file://$2   --parameters file://$2  --region=us-east-2
+aws cloudformation create-stack --stack-name $1 --template-body file://$2   --parameters file://$3  --region=us-east-2
